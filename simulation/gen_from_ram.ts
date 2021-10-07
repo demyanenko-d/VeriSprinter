cae21b0a6a8b01a1bae6f03b72b9a069f5255b55
demyanenko-d <76960752+demyanenko-d@users.noreply.github.com>
2021-10-07 18:01:53 +0300

    поправил генератор

--- simulation/gen_from_ram.ts
+++ simulation/gen_from_ram.ts
@@ -46,7 +46,7 @@ enum VState {
 let cur_state: VState = VState.None;
 let va = 0;
 let tmp_modes = 0;
-let attr, pix;
+let attr = 0, pix = 0;
 let mode0 = 0, mode1 = 0, mode2 = 0, mode3 = 0;
 let oldmode0 = -1;
 
@@ -58,9 +58,11 @@ let bord = false;
 let blank = false;
 let intx = false;
 
+let curr_r = 0, curr_g = 0, curr_b = 0;
+let pal_index = 0, pal_num = 0;
 
 for (let cnt_y = 0; cnt_y < scr_h; cnt_y++) {
-    const scr_line = (cnt_y * scr_w) << 2;
+    const scr_line = cnt_y * scr_w * 2;
     for (let cnt_x = 0; cnt_x < scr_w; cnt_x++) {
 
         const ray_cntx = cnt_x >> 3;
@@ -87,7 +89,7 @@ for (let cnt_y = 0; cnt_y < scr_h; cnt_y++) {
                 }
                 case 4: {
                     pixel_14m |= 1;
-                    cur_state = (pixel_7m & 0x3) == 0 ? VState.RdModes : VState.None;
+                    cur_state = VState.RdModes;
                     //debug && console.log("vfase: RdModes");
                     break
                 }
@@ -106,14 +108,14 @@ for (let cnt_y = 0; cnt_y < scr_h; cnt_y++) {
 
             switch (cur_state) {
                 case VState.RdModes: {
-                    va = (pgm << 17) | (ray_cntx << 11) | ((pixel_7m >> 2) << 10) | 0x300 | ((cnt_y >> 3) << 2);
+                    va = (pgm << 17) | (ray_cntx << 11) | (((pixel_7m >> 2)) << 10) | 0x300 | ((cnt_y >> 3) << 2);
                     tmp_modes = ram[va >> 2];
-                    console.log("modes: " + tmp_modes.toString(16));
 
+                    //console.log("modes: " + tmp_modes.toString(16));
                     //debug && console.log("vstate: RdModes");
                     //debug && console.log("cnt_y[8:3]: " + (cnt_y >> 3).toString(16) + " CT5: " + (pixel_7m >> 2) + " CTH[5:3]: " +  ray_cntx.toString(16));
-                    debug && print_addr(va);
-                    debug && debug_modes(tmp_modes);
+                    //debug && print_addr(va);
+                    //debug && debug_modes(tmp_modes);
 
                     break;
                 }
@@ -127,7 +129,7 @@ for (let cnt_y = 0; cnt_y < scr_h; cnt_y++) {
                     } else { // gfx
                         //	Graf adress
 
-                        let posy = (cnt_y & 7) ^ 7; // (mode2 >> 2) != 0 ? ((cnt_y & 7)^7) : (((mode2 & 1) << 2) | (((cnt_y >> 1) & 3) ^ 3));
+                        let posy = (cnt_y & 7);  // (mode2 >> 2) != 0 ? ((cnt_y & 7)^7) : (((mode2 & 1) << 2) | (((cnt_y >> 1) & 3) ^ 3));
                         let posx = pixel_7m & 7; //  (mode2 >> 2) != 0 ? (pixel_7m & 7) :  (((mode2 & 2) << 1) | ;
 
                         if ((mode2 >> 2) != 0) {
@@ -138,7 +140,14 @@ for (let cnt_y = 0; cnt_y < scr_h; cnt_y++) {
                             if ((mode2 & 2) != 0) posx |= 0x4;
                         }
 
-                        va = ((mode1 >> 3) << 13) | (posy << 10) | ((mode0 & 0x0f) << 6) | ((mode1 & 7) << 3) | posx;
+                        va =
+                            ((mode1 >> 3) << 13) |
+                            (posy << 10) |
+                            ((mode0 & 0x0f) << 6) |
+                            ((mode1 & 7) << 3) |
+                            posx;
+
+                        va ^= 1 << 4;
                     }
 
                     // mux
@@ -160,11 +169,28 @@ for (let cnt_y = 0; cnt_y < scr_h; cnt_y++) {
                 }
 
                 case VState.RdPallete: {
-                    
+                    va = (pal_index << 10) | 0x3e0 | (pal_num << 2);
+                    let tmp = ram[va >> 2];
+                    curr_r = (tmp >> 0) & 0xff;
+                    curr_g = (tmp >> 8) & 0xff;
+                    curr_b = (tmp >> 16) & 0xff;
+
+                    break;
                 }
             }
 
-            if (vfase == 1) {
+            if (mode_gfx) {
+                if (res640) {
+                    //pal_index = (pixel_7m & 4) ?  (attr >> 4) : (attr & 4);
+                    pal_index = (attr >> 4);
+                } else
+                    pal_index = attr;
+
+            } else {
+
+            }
+
+            if (vfase == 6) {
                 mode0 = (tmp_modes >> 0) & 0xff;
                 mode1 = (tmp_modes >> 8) & 0xff;
                 mode2 = (tmp_modes >> 16) & 0xff;
@@ -183,14 +209,34 @@ for (let cnt_y = 0; cnt_y < scr_h; cnt_y++) {
 
                     debug && console.log(`bord:${bord} blank:${blank} intx:${intx} res320:${res320} res640:${res640} txt:${mode_text} gfx:${mode_gfx}`);
                 };
+
                 oldmode0 = mode0;
             }
+
+            if (vfase == 1) {
+                let pointer = (scr_line + cnt_x * 2) << 2;
+                png.data[pointer + 0] = curr_r;
+                png.data[pointer + 1] = curr_g;
+                png.data[pointer + 2] = curr_b;
+                png.data[pointer + 3] = 0xff;
+            }
+
+            if (vfase == 4) {
+                let pointer = (scr_line + cnt_x * 2 + 1) << 2;
+                png.data[pointer + 0] = curr_r;
+                png.data[pointer + 1] = curr_g;
+                png.data[pointer + 2] = curr_b;
+                png.data[pointer + 3] = 0xff;
+            }
+
+
         }
 
 
     }
-    process.exit();
+    if (cnt_y > 140) break;
 }
 
 
 fs.writeFileSync("ram.png", PNG.sync.write(png));
+
